9d93749b15003faeeb3dd0b89b49e79bb5b2586d
ritzzh <ritesh.singh.19a@gmail.com>
2024-11-04 18:40:09 +0530

    added hertz custom pipe to child component

--- src/app/hertz.pipe.ts
+++ src/app/hertz.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HertzPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: string|undefined): string {
     return value+"MHz";
   }
 
